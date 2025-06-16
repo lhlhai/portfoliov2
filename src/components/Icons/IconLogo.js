55cdb0e69f13f5abc4a01713aca28611fe7a976d
lhlhai <lhlhai.spkt@gmail.com>
2025-06-16 17:29:07 +0700

    Update IconLogo.js

--- src/components/Icons/IconLogo.js
+++ src/components/Icons/IconLogo.js
@@ -6,14 +6,8 @@ const IconLogo = ({ theme, width = 36.581, height = 50.186, ...props }) => {
   return (
     <svg width={width} height={height} viewBox="0 0 106.581 120.186" {...props}>
       <g fill={theme.brand.primary}>
-        {/* H Letter */}
-        <rect x="10" y="20" width="8" height="80" />
-        <rect x="10" y="56" width="32" height="8" />
-        <rect x="34" y="20" width="8" height="80" />
-        
-        {/* L Letter */}
-        <rect x="60" y="20" width="8" height="80" />
-        <rect x="60" y="92" width="36" height="8" />
+        <path d="M10 20h8v80h-8V20zm0 36h32v8H10v-8zm24-36h8v80h-8V20z" />
+        <path d="M60 20h8v80h-8V20zm0 72h36v8H60v-8z" />
       </g>
     </svg>
   );
